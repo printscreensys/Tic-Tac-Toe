cbb077cfe3127b592953500d5d09b41c089d5849
printscreensys <ilyasov.yasha@hotmail.com>
2021-01-29 13:34:27 +0300

    Added easy level

--- main.py
+++ main.py
@@ -1,14 +1,12 @@
+from random import choice
+
 class Game:
     def __init__(self, level):
         self.level = level
     
-    def side(self, user):
-        if user.count('X') == user.count('O'):
-            side = 'X'
-        else:
-            side = 'O'
-        
-        return side
+    def side(self, user = 'X'):
+
+        return user
 
     def elements(self, user):
         user = user.replace('_',' ')
@@ -20,8 +18,7 @@ class Game:
     
         return elements
 
-    def move(self, x,y,elements):
-        global user
+    def move(self, x, y, elements, user):
         elements[x-1][y-1] = self.side(user)
 
         return elements
@@ -47,39 +44,53 @@ class Game:
         if elements[2][0] == elements[1][1] and elements[1][1] == elements[0][2] and elements[2][0] != ' ':
             return elements[2][0]
 
-game = Game(None)
-user = input()
-print('Enter the cells:'+user)
+    def free_cells(self, elements):
+        free_cells = []
+        for i in range(3):
+            for j in range(3):
+                if elements[i][j] == ' ':
+                    free_cells.append((i+1,j+1))
+        
+        return free_cells
+
+game = Game('easy')
+user = '_________'
 elements = game.elements(user)
 for row in game.board(elements):
     print(row)
-
-correct = False
-while correct == False:
-    try:
-        x,y = input().split()
-        print('Enter the coordinates:'+x+' '+y)
-        if x.isdigit() and y.isdigit():
-            x = int(x)
-            y = int(y)
-            if {x,y}.issubset({1,2,3}):
-                target = elements[x-1][y-1]
-                if target != ' ':
-                    print('This cell is occupied! Choose another one!')
+while game.check_win(elements) == None or game.free_cells(elements) != []:
+    correct = False
+    while correct == False:
+        try:
+            x,y = input().split()
+            print('Enter the coordinates:'+x+' '+y)
+            if x.isdigit() and y.isdigit():
+                x = int(x)
+                y = int(y)
+                if {x,y}.issubset({1,2,3}):
+                    target = elements[x-1][y-1]
+                    if target != ' ':
+                        print('This cell is occupied! Choose another one!')
+                    else:
+                        elements = game.move(x,y,elements, 'X')
+                        for row in game.board(elements):
+                            print(row)
+                        correct = True
                 else:
-                    elements = game.move(x,y,elements)
-                    for row in game.board(elements):
-                        print(row)
-                    correct = True
+                    print('Coordinates should be from 1 to 3!')
             else:
-                print('Coordinates should be from 1 to 3!')
-        else:
+                print('You should enter numbers!')
+        except ValueError:
             print('You should enter numbers!')
-    except ValueError:
-        print('You should enter numbers!')
+    if game.free_cells(elements) != []:
+        print('Making move level "easy"')
+        elements = game.move(*choice(game.free_cells(elements)), elements, 'O')
+        for row in game.board(elements):
+            print(row)
+    else:
+        break
+    
 if game.check_win(elements) != None:
     print(game.check_win(elements)+' wins')
-elif user.count('_')>1:
-    print('Game not finished')
 else:
     print('Draw')

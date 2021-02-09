904edd4e273d98373246d8cc295cebaddaae0199
printscreensys <ilyasov.yasha@hotmail.com>
2021-02-09 12:22:16 +0300

    New features: Bots could play themselfs. Added start and exit commands. Further will be added medium and hard levels

---
+++ classes.py
@@ -0,0 +1,70 @@
+class Game:
+    def __init__(self, command, level1, level2):
+        self.command = command
+        self.level1 = level1
+        self.level2 = level2
+ 
+    def elements(self, user):
+        user = user.replace('_',' ')
+        elements = []
+        for i in range(3):
+            elements.append([])
+        for i in range(3):
+            elements[i] = [char for char in user[3*i:3*(i+1)]]
+    
+        return elements
+    
+    def board(self, elements):
+        out = []
+        out.append('---------')
+        for row in elements:
+            out.append('| '+' '.join(row)+' |')
+        out.append('---------')
+ 
+        return out
+    
+    def check_win(self, elements):
+        for i in range(3):
+            if elements[i][0] == elements[i][1] and elements[i][1] == elements[i][2] and elements[i][0] != ' ':
+                return elements[i][0]
+        for j in range(3):
+            if elements[0][j] == elements[1][j] and elements[1][j] == elements[2][j] and elements[0][j] != ' ':
+                return elements[0][j]
+        if elements[0][0] == elements[1][1] and elements[1][1] == elements[2][2] and elements[0][0] != ' ':
+            return elements[0][0]
+        if elements[2][0] == elements[1][1] and elements[1][1] == elements[0][2] and elements[2][0] != ' ':
+            return elements[2][0]
+    
+    class Player:
+        def __init__(self, side, level):
+            self.side = side
+            self.level = level
+        
+        def check_input(self, x, y, elements):
+            occupied = None
+            out_of_range = None
+            not_numbers = None
+            if x.isdigit() and y.isdigit():
+                x = int(x)
+                y = int(y)
+                if {x,y}.issubset({1,2,3}):
+                    if elements[x-1][y-1] != ' ':
+                        return 'This cell is occupied! Choose another one!'
+                else:
+                    return 'Coordinates should be from 1 to 3!'
+            else:
+                return 'You should enter numbers!'
+            
+        def move(self, x, y, elements):
+            elements[x-1][y-1] = self.side
+ 
+            return elements
+        
+        def free_cells(self, elements):
+            free_cells = []
+            for i in range(3):
+                for j in range(3):
+                    if elements[i][j] == ' ':
+                        free_cells.append((i+1,j+1))
+            
+            return free_cells
--- main.py
+++ main.py
@@ -1,96 +1,81 @@
 from random import choice
-
-class Game:
-    def __init__(self, level):
-        self.level = level
-    
-    def side(self, user = 'X'):
-
-        return user
-
-    def elements(self, user):
-        user = user.replace('_',' ')
-        elements = []
-        for i in range(3):
-            elements.append([])
-        for i in range(3):
-            elements[i] = [char for char in user[3*i:3*(i+1)]]
-    
-        return elements
-
-    def move(self, x, y, elements, user):
-        elements[x-1][y-1] = self.side(user)
-
-        return elements
-
-    def board(self, elements):
-        out = []
-        out.append('---------')
-        for row in elements:
-            out.append('| '+' '.join(row)+' |')
-        out.append('---------')
-
-        return out
-
-    def check_win(self, elements):
-        for i in range(3):
-            if elements[i][0] == elements[i][1] and elements[i][1] == elements[i][2] and elements[i][0] != ' ':
-                return elements[i][0]
-        for j in range(3):
-            if elements[0][j] == elements[1][j] and elements[1][j] == elements[2][j] and elements[0][j] != ' ':
-                return elements[0][j]
-        if elements[0][0] == elements[1][1] and elements[1][1] == elements[2][2] and elements[0][0] != ' ':
-            return elements[0][0]
-        if elements[2][0] == elements[1][1] and elements[1][1] == elements[0][2] and elements[2][0] != ' ':
-            return elements[2][0]
-
-    def free_cells(self, elements):
-        free_cells = []
-        for i in range(3):
-            for j in range(3):
-                if elements[i][j] == ' ':
-                    free_cells.append((i+1,j+1))
-        
-        return free_cells
-
-game = Game('easy')
-user = '_________'
-elements = game.elements(user)
-for row in game.board(elements):
-    print(row)
-while game.check_win(elements) == None or game.free_cells(elements) != []:
-    correct = False
-    while correct == False:
+from classes import Game
+command = input().split()
+print('Input command: '+ ' '.join(command))
+while ' '.join(command) != 'exit':
+    start = False
+    exit_ = False
+    while not start:
         try:
-            x,y = input().split()
-            print('Enter the coordinates:'+x+' '+y)
-            if x.isdigit() and y.isdigit():
-                x = int(x)
-                y = int(y)
-                if {x,y}.issubset({1,2,3}):
-                    target = elements[x-1][y-1]
-                    if target != ' ':
-                        print('This cell is occupied! Choose another one!')
-                    else:
-                        elements = game.move(x,y,elements, 'X')
+            game = Game(*command)
+            elements = game.elements('_________')
+            for row in game.board(elements):
+                print(row)
+            start = True
+        except TypeError:
+            print('Bad parameters!')
+            command = input().split()
+            print('Input command: '+ ' '.join(command))
+            if command == ['exit']:
+                exit_ = True
+                break
+    if exit_:
+        break
+    
+    player1 = game.Player('X', game.level1)
+    player2 = game.Player('O', game.level2)
+ 
+    while game.check_win(elements) == None and player1.free_cells(elements) != []:
+        if player1.level == 'user':
+            print('Enter the coordinates:')
+            correct = False
+            while correct == False:
+                try:
+                    x,y = input().split()
+                    print('Enter the coordinates: '+x+' '+y)
+                    if player1.check_input(x,y,elements) == None:
+                        x = int(x)
+                        y = int(y)
+                        elements = player1.move(x,y,elements)
                         for row in game.board(elements):
                             print(row)
                         correct = True
-                else:
-                    print('Coordinates should be from 1 to 3!')
+                    else:
+                        print(player1.check_input(x,y,elements))
+                except ValueError:
+                    print('You should enter numbers!')
+        else:
+            print('Making move level "easy"')
+            elements = player1.move(*choice(player1.free_cells(elements)), elements)
+            for row in game.board(elements):
+                print(row)
+        if player1.free_cells(elements) != [] and game.check_win(elements) == None:
+            if player2.level == 'user':
+                print('Enter the coordinates:')
+                correct = False
+                while correct == False:
+                    try:
+                        x,y = input().split()
+                        print('Enter the coordinates: '+x+' '+y)
+                        if player2.check_input(x,y,elements) == None:
+                            x = int(x)
+                            y = int(y)
+                            elements = player2.move(x,y,elements)
+                            for row in game.board(elements):
+                                print(row)
+                            correct = True
+                        else:
+                            print(player2.check_input(x,y,elements))
+                    except ValueError:
+                        print('You should enter numbers!')
             else:
-                print('You should enter numbers!')
-        except ValueError:
-            print('You should enter numbers!')
-    if game.free_cells(elements) != []:
-        print('Making move level "easy"')
-        elements = game.move(*choice(game.free_cells(elements)), elements, 'O')
-        for row in game.board(elements):
-            print(row)
+                print('Making move level "easy"')
+                elements = player2.move(*choice(player2.free_cells(elements)), elements)
+                for row in game.board(elements):
+                    print(row)
+    if game.check_win(elements) != None:
+        print(game.check_win(elements)+' wins')
     else:
-        break
-    
-if game.check_win(elements) != None:
-    print(game.check_win(elements)+' wins')
-else:
-    print('Draw')
+        print('Draw')
+    command = input().split()
+    print('Input command: '+ ' '.join(command))
